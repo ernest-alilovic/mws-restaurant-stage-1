c4f901bd603d0438a659b689a38938c3d7a326db
Ernest Alilovic <ernest.alilovic@gmail.com>
2018-08-04 09:42:05 +0200

    Fixed service worker

--- service_worker.js
+++ service_worker.js
@@ -1,37 +1,44 @@
-let staticCacheName = 'restaurant-static-v1';
+let staticCacheName = 'restaurant-cache';
+let urlisToCache = [
+	[
+		'./',
+		'./index.html',
+		'./restaurant.html',
+		'./css/styles.css',
+		'./data/restaurants.json',
+		'./js/dbhelper.js',
+		'./js/main.js',
+		'./js/restaurant_info.js',
+		'./js/sw_register.js',
+		'./img/1.jpg',
+		'./img/2.jpg',
+		'./img/3.jpg',
+		'./img/4.jpg',
+		'./img/5.jpg',
+		'./img/6.jpg',
+		'./img/7.jpg',
+		'./img/8.jpg',
+		'./img/9.jpg',
+		'./img/10.jpg'
+];
 
+/**
+ * Installation of service worker
+ */
 self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open(staticCacheName).then(function(cache) {
-			return cache.addAll([
-				'./',
-				'./index.html',
-				'./restaurant.html',
-				'./css/styles.css',
-				'./data/restaurants.json',
-				'./js/dbhelper.js',
-				'./js/main.js',
-				'./js/restaurant_info.js',
-				'./js/sw_register.js',
-				'./img/1.jpg',
-				'./img/2.jpg',
-				'./img/3.jpg',
-				'./img/4.jpg',
-				'./img/5.jpg',
-				'./img/6.jpg',
-				'./img/7.jpg',
-				'./img/8.jpg',
-				'./img/9.jpg',
-				'./img/10.jpg'
-			]);
+			return cache.addAll(urlisToCache);
 		})
 	);
 });
 
+/**
+ * Activation of service worker
+ */
 self.addEventListener('activate', function(event) {
 	event.waitUntil(
-		caches.keys()
-		.then(function(cacheNames) {
+		caches.keys().then(function(cacheNames) {
 			return Promise.all(
 				cacheNames.filter(function(cacheName) {
 					return cacheName.startsWith('restaurant-') &&
@@ -44,6 +51,9 @@ self.addEventListener('activate', function(event) {
 	);
 })
 
+/**
+ * Fetching for offline content viewing
+ */
 self.addEventListener('fetch', function(event) {
 	event.respondWith(
 		caches.match(event.request)
